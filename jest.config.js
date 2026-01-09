2d19092545af241ad6c7049e682560725eb4d6a3
alenayuskova <159901050+alenayuskova@users.noreply.github.com>
2026-01-09 12:49:51 +0200

    11

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,21 @@
 export default {
   testEnvironment: 'node',
-  transform: {}
+  transform: {},
+
+  // ✅ ВКЛЮЧАЄМО COVERAGE
+  collectCoverage: true,
+
+  // ✅ ЗВІДКИ БРАТИ ФАЙЛИ ДЛЯ ПОКРИТТЯ
+  collectCoverageFrom: [
+    'libs/**/*.js'
+  ],
+
+  // (необовʼязково, але дуже корисно)
+  coverageReporters: ['text', 'lcov'],
+
+  // щоб не рахувало тести і node_modules
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.test.js'
+  ]
 };
